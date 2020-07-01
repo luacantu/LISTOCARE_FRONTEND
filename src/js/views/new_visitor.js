1c796384838cd5575768fb25d9edcad422d01a90
luisachila0620 <luisarchila038@gmail.com>
2020-07-01 00:28:58 +0000

    playing again with it

--- src/js/views/new_visitor.js
+++ src/js/views/new_visitor.js
@@ -3,10 +3,13 @@ import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
 
 export const new_visitor = () => (
-	<div className="text-center-mt-5">
-		<div>
-			<img src="https://www.redcross.org/content/dam/icons/disaster-services/erv/ERV-1000x1000-G-Pl.png.img.png" />
-		</div>
-		<h1>hello world</h1>
+	<div className="text-center mt-5">
+		<button>
+			<h1>hello world</h1>
+		</button>
+
+		<p classnme="rojo">this is just an example on how to practice the new website design</p>
+
+		<img src="https://www.redcross.org/content/dam/icons/disaster-services/erv/ERV-1000x1000-G-Pl.png.img.png" />
 	</div>
 );
