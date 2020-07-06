d6a304c12001e78571f60504cc59d81cdff74a4f
luisachila0620 <luisarchila038@gmail.com>
2020-07-06 01:13:31 +0000

    finishing fist top front end

--- src/js/views/contactanos.js
+++ src/js/views/contactanos.js
@@ -2,24 +2,24 @@ import React from "react";
 
 export const Contactanos = () => {
 	return (
-		<div classNameName="card bg-dark text-white">
-			<img
-				src="https://cdn5.vectorstock.com/i/1000x1000/72/29/abstract-blue-and-white-polygon-background-vector-9317229.jpg"
-				classNameName="card-img"
-				alt="..."
-			/>
-			<div classNameName="card-img-overlay">
-				<h5 classNameName="card-title">Tu consultorio virtual durante la pandemia covid-19</h5>
-				<p classNameName="card-text">
-					Listocare lo ayuda a brindar una exelente atencion al paciente mientras desarrolla una practica
-					saludable.
-				</p>
-				<p classNameName="card-text">Last updated 3 mins ago</p>
+		<div>
+			<div className="card bg-dark text-white">
+				<img
+					src="https://dm0qx8t0i9gc9.cloudfront.net/thumbnails/video/itCjTBE/blue-abstract-moving-flowing-waves-on-white-background-blurred-smooth-design-video-animation-ultra-hd-4k-3840x2160_rffdhgnp_thumbnail-1080_05.png"
+					className="card-img"
+					alt="..."
+				/>
+				<div className="card-img-overlay">
+					<h5 className="card-title">Tu consultorio virtual durante la pandemia covid-19</h5>
+					<p className="card-text">
+						Listocare lo ayuda a brindar una exelente atencion al paiente mientras desarrolla una practica
+						saludable..
+					</p>
+					<p className="card-text">
+						<button className="Registrate">Registrate</button>
+					</p>
+				</div>
 			</div>
 		</div>
-        
-            
-    
-
 	);
 };
