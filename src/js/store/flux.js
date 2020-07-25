39b210a0be432ef6c2edf0356a17a925ea72f00c
Paolo Lucano <paololucanodeveloper@gmail.com>
2020-07-25 16:36:24 +0000

    updated flux

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,6 +1,10 @@
+const listocareBackendUrl = "https://3000-dccb160b-118f-4318-a1aa-bb2957d1c89d.ws-us02.gitpod.io";
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
+			token: null,
+			doctorId: null,
+
 			demo: [
 				{
 					title: "FIRST",
@@ -15,6 +19,44 @@ const getState = ({ getStore, getActions, setStore }) => {
 			]
 		},
 		actions: {
+			login: async (email, password) => {
+				const response = await fetch(`${listocareBackendUrl}/login`, {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json"
+					},
+					body: JSON.stringify({
+						email: email,
+						password: password
+					})
+				});
+				const body = await response.json();
+				// console.log("body", body);
+				if (response.status == 200) {
+					// console.log("test");
+					setStore({ token: body.jwt, doctorId: body.doctor_id });
+				} else setStore({ token: null, doctorId: null });
+			},
+			doctorSignUp: async (name, lastName, username, email, password) => {
+				const response = await fetch(`${listocareBackendUrl}/medicalstaff`, {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json"
+					},
+					body: JSON.stringify({
+						first_name: name,
+						last_name: lastName,
+						email: email,
+						username: username,
+						password: password
+					})
+				});
+				if (response.ok) {
+					return true;
+				} else {
+					return false;
+				}
+			},
 			// Use getActions to call a function within a fuction
 			exampleFunction: () => {
 				getActions().changeColor(0, "green");
